5d11d3e044c0c1929ab73d50f6944bf000ebada6
patrickC20 <pcockrill2@gmail.com>
2021-07-11 19:04:24 -0400

    added html source

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/CS-PCockrill/bookings-app/pkg/config"
 	"github.com/CS-PCockrill/bookings-app/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
@@ -18,5 +18,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
